a75e601a3979bff2aa6c3084a30e8a6671400608
Tinsa <tinsa.baby@whitehatjr.com>
2024-02-28 20:33:44 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Form from './Form';
 import { createContext, useState } from 'react';
 import Sample from './Sample.jsx';
+import Intro from './components/Intro.jsx';
 
 export const themeContext=createContext()
 function App() {
@@ -11,8 +12,9 @@ function App() {
   return (
     <themeContext.Provider value={[theme,setTheme]}>
     <div className="App">
-      <Form/>
-      <Sample/>
+      {/* <Form/>
+      <Sample/> */}
+      <Intro/>
     </div>
     </themeContext.Provider>
   );
